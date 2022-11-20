ad7b64ab443c5ff29e41d6384377592100617d78
Austin Hunt <austinsketches@gmail.com>
2022-11-19 19:24:46 -0500

    update models to include twilio settings & verbose names

--- GoogleSharePointMigrationAssistant/web/models.py
+++ GoogleSharePointMigrationAssistant/web/models.py
@@ -20,27 +20,34 @@ class Migration(models.Model):
 
 class AdministrationSettings(models.Model):
     # For OAuth-driven migrations (user-driven)
-    google_oauth_client_id = models.CharField(max_length=100, null=True, blank=True)
-    google_oauth_project_id = models.CharField(max_length=50)
-    google_oauth_auth_uri = models.URLField(null=True, blank=True)
-    google_oauth_token_uri = models.URLField(null=True, blank=True)
-    google_oauth_auth_provider_x509_cert_url = models.URLField(null=True, blank=True)
-    google_oauth_auth_client_secret = models.CharField(max_length=50, null=True, blank=True)
+    google_oauth_client_id = models.CharField(max_length=100, null=True, blank=True, verbose_name='Google Service Account - Client ID')
+    google_oauth_project_id = models.CharField(max_length=50, verbose_name='Google Service Account - Project ID')
+    google_oauth_auth_uri = models.URLField(null=True, blank=True, verbose_name='Google Service Account - Auth URI')
+    google_oauth_token_uri = models.URLField(null=True, blank=True, verbose_name='Google Service Account - Token URI')
+    google_oauth_auth_provider_x509_cert_url = models.URLField(null=True, blank=True, verbose_name='Google Service Account - Auth Provider X509 Cert URL')
+    google_oauth_auth_client_secret = models.CharField(max_length=50, null=True, blank=True, verbose_name='Google Service Account - Client Secret')
     
     # For SVC-account driven migrations (admin-driven). More vulnerable, requires a highly privileged, centralized service account.
-    google_service_account_auth_project_id = models.CharField(max_length=128, null=True, blank=True)
-    google_service_account_auth_private_key_id = models.CharField(max_length=128, null=True, blank=True)
-    google_service_account_auth_private_key = models.TextField(null=True, blank=True)
-    google_service_account_auth_client_email = models.EmailField(null=True, blank=True)
-    google_service_account_auth_client_id = models.CharField(max_length=32, null=True, blank=True)
+    google_service_account_auth_project_id = models.CharField(max_length=128, null=True, blank=True, verbose_name='Google Service Account - Project ID')
+    google_service_account_auth_private_key_id = models.CharField(max_length=128, null=True, blank=True, verbose_name='Google Service Account - Private Key ID')
+    google_service_account_auth_private_key = models.TextField(null=True, blank=True, verbose_name='Google Service Account - Private Key')
+    google_service_account_auth_client_email = models.EmailField(null=True, blank=True, verbose_name='Google Service Account - Client Email')
+    google_service_account_auth_client_id = models.CharField(max_length=32, null=True, blank=True, verbose_name='Google Service Account - Client ID')
 
     # SMTP-based email notifications.
-    smtp_send_email_notifications = models.BooleanField(default=False)
-    smtp_username = models.CharField(max_length=64, null=True, blank=True)
-    smtp_password = models.CharField(max_length=64, null=True, blank=True)
-    smtp_port = models.IntegerField(default=587, null=True, blank=True)
-    smtp_server = models.CharField(max_length=128, null=True, blank=True)
+    smtp_enable_email_notifications = models.BooleanField(default=False, verbose_name='Do you want to enable email notifications (on migration completion)? If so, the below fields will be required.')
+    smtp_username = models.CharField(max_length=64, null=True, blank=True, verbose_name='SMTP Username')
+    smtp_password = models.CharField(max_length=64, null=True, blank=True, verbose_name='SMTP Password')
+    smtp_port = models.IntegerField(default=587, null=True, blank=True, verbose_name='SMTP Port (Default is 587)')
+    smtp_server = models.CharField(max_length=128, null=True, blank=True, verbose_name='SMTP Server / Host (e.g. smtp.office365.com)')
     smtp_subject = models.TextField(default="Google Drive to SharePoint Migration Complete", null=True, blank=True)
+
+    # Twilio 
+    twilio_enable_sms_notifications = models.BooleanField(default=False, verbose_name='Do you want to enable SMS notifications for users? If so, the Twilio settings below will be required.')
+    twilio_messaging_service_sid = models.CharField(max_length=32, null=True, blank=True, verbose_name='Provide your Twilio Messaging Service SID')
+    twilio_account_sid = models.CharField(max_length=32, null=True, blank=True, verbose_name='Provide your Twilio Account SID (from your Twilio account dashboard)')
+    twilio_account_auth_token = models.CharField(max_length=32, null=True, blank=True, verbose_name='Provide your Twilio Account Auth Token (from your Twilio account dashboard)')
+
 
 class AdministrationSettings_ListAttributeItem(models.Model):
     """ Cannot really store a list of strings well in a single column so 
